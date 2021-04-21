7bccddc253dec73335d7ed7916f44969921f4a12
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-04-21 10:57:10 -0300

    Criando DTO para omitir livros em categoria

--- src/main/java/com/sueliton/livrariavirtual/resources/CategoriaResource.java
+++ src/main/java/com/sueliton/livrariavirtual/resources/CategoriaResource.java
@@ -2,15 +2,19 @@ package com.sueliton.livrariavirtual.resources;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sueliton.livrariavirtual.domain.Categoria;
+import com.sueliton.livrariavirtual.dtos.CategoriaDTO;
 import com.sueliton.livrariavirtual.services.CategoriaService;
 
 @RestController
@@ -21,15 +25,25 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Categoria>> findAll() {
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Optional<Categoria> obj = service.find(id);
 		return ResponseEntity.ok(obj);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<Categoria>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "id") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
--- src/main/java/com/sueliton/livrariavirtual/services/CategoriaService.java
+++ src/main/java/com/sueliton/livrariavirtual/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sueliton.livrariavirtual.domain.Categoria;
@@ -28,5 +31,12 @@ public class CategoriaService {
 		return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrato! Id: " + id + ", Tipo: " + Categoria.class.getName())));
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	
 
 }

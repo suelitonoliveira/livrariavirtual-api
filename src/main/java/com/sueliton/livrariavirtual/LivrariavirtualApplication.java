8fde861244bee4f15d6a1589f7fc0f8c93202091
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-04-21 08:36:17 -0300

    Projeto criado

---
+++ src/main/java/com/sueliton/livrariavirtual/LivrariavirtualApplication.java
@@ -0,0 +1,13 @@
+package com.sueliton.livrariavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariavirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sueliton/livrariavirtual/LivrariavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sueliton.livrariavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

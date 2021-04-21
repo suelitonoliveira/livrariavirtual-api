bcf971e3e9dfd3cb09465bba337474d0e30a8bd1
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-04-21 09:51:27 -0300

    Criado perfil test

--- src/main/java/com/sueliton/livrariavirtual/LivrariavirtualApplication.java
+++ src/main/java/com/sueliton/livrariavirtual/LivrariavirtualApplication.java
@@ -1,40 +1,13 @@
 package com.sueliton.livrariavirtual;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.sueliton.livrariavirtual.domain.Categoria;
-import com.sueliton.livrariavirtual.domain.Livro;
-import com.sueliton.livrariavirtual.repositories.CategoriaRepository;
-import com.sueliton.livrariavirtual.repositories.LivroRepository;
-
 @SpringBootApplication
-public class LivrariavirtualApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+public class LivrariavirtualApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LivrariavirtualApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-
 	}
 
 }
---
+++ src/main/java/com/sueliton/livrariavirtual/service/DBService.java
@@ -0,0 +1,43 @@
+package com.sueliton.livrariavirtual.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sueliton.livrariavirtual.domain.Categoria;
+import com.sueliton.livrariavirtual.domain.Livro;
+import com.sueliton.livrariavirtual.repositories.CategoriaRepository;
+import com.sueliton.livrariavirtual.repositories.LivroRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public void instanciaBaseDeDados() {
+		Categoria cat1 = new Categoria(null, "informática", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", " Livros de Ficção Científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+		Categoria cat4 = new Categoria(null, "Gramática", "Livros de gramática");
+		
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat3);
+		Livro l5 = new Livro(null, "Nova gramática", "Lindley Cintra", "Lorem ipsum", cat4);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1,l2));
+		cat2.getLivros().addAll(Arrays.asList(l3));
+		cat3.getLivros().addAll(Arrays.asList(l4));
+		cat4.getLivros().addAll(Arrays.asList(l5));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	}
+
+}
